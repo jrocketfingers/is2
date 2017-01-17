2fd3a28190e3f354501fa5f3e9e2920211c818db
Johnny Rocketfingers <developer94@gmail.com>
2017-01-17 12:21:57 +0100

    Initial commit.

---
+++ db/models.py
@@ -0,0 +1,34 @@
+import peewee
+
+class Article(peewee.Model):
+    pass
+
+class Type(peewee.Model):
+    pass
+
+class Size(peewee.Model):
+    pass
+
+class Color(peewee.Model):
+    pass
+
+class Customer(peewee.Model):
+    pass
+
+class Seller(peewee.Model):
+    pass
+
+class District(peewee.Model):
+    pass
+
+class City(peewee.Model):
+    pass
+
+class Offer(peewee.Model):
+    pass
+
+class Order(peewee.Model):
+    pass
+
+class OfferOrder(peewee.Model):
+    pass
